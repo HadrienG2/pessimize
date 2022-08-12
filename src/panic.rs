72d18e9477b29778095bb538219ef226d137d2f4
Hadrien Grasland <hadrien.grasland@gmx.fr>
2022-08-12 17:11:34 +0200

    Add tests for core::panic

--- src/panic.rs
+++ src/panic.rs
@@ -10,4 +10,48 @@ pessimize_newtypes!(
     }
 );
 
-// FIXME: Can't test AssertUnwindSafe as it doesn't implement the right traits
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        pessimize_newtypes,
+        tests::{test_unoptimized_value_type, test_value_type},
+    };
+
+    #[derive(Debug, Default)]
+    struct TestableAssertUnwindSafe(AssertUnwindSafe<isize>);
+    //
+    impl TestableAssertUnwindSafe {
+        fn new(inner: isize) -> Self {
+            Self(AssertUnwindSafe(inner))
+        }
+    }
+    //
+    impl Clone for TestableAssertUnwindSafe {
+        fn clone(&self) -> Self {
+            Self::new((self.0).0)
+        }
+    }
+    //
+    impl PartialEq for TestableAssertUnwindSafe {
+        fn eq(&self, other: &Self) -> bool {
+            (self.0).0 == (other.0).0
+        }
+    }
+    //
+    pessimize_newtypes!( allow(missing_docs) { TestableAssertUnwindSafe{ AssertUnwindSafe<isize> } } );
+    //
+    #[test]
+    fn assert_unwind_safe() {
+        test_value_type(
+            TestableAssertUnwindSafe::new(isize::MIN),
+            TestableAssertUnwindSafe::new(isize::MAX),
+        );
+    }
+    //
+    #[test]
+    #[ignore]
+    fn assert_unwind_safe_optim() {
+        test_unoptimized_value_type::<TestableAssertUnwindSafe>();
+    }
+}
