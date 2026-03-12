28a7e894203b052fc25812e6ba1919ea0186f92f
Hadrien G. <knights_of_ni@gmx.com>
2026-03-12 08:21:32 +0100

    Narrow usage of target_feature for portable_simd
    Due to its idiotic focus on variable-length SIMD, RISC-V is currently not
    amenable to SIMD data pessimization, and thus is not tested via portable_simd

--- src/lib.rs
+++ src/lib.rs
@@ -75,7 +75,14 @@
 //! - It needs a lot of tricky unsafe code.
 
 #![cfg_attr(not(any(feature = "std", test)), no_std)]
-#![cfg_attr(feature = "nightly", feature(doc_cfg, portable_simd, ptr_metadata))]
+#![cfg_attr(feature = "nightly", feature(doc_cfg, ptr_metadata))]
+#![cfg_attr(
+    all(
+        feature = "nightly",
+        not(any(target_arch = "riscv32", target_arch = "riscv64"))
+    ),
+    feature(portable_simd)
+)]
 #![cfg_attr(feature = "default_impl", allow(incomplete_features))]
 #![cfg_attr(feature = "default_impl", feature(specialization))]
 #![deny(missing_docs)]
